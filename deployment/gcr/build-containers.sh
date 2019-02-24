8d217f04d342b3fdec93a96959a53c8b73eebd1c
Piotr Walkusz <piotrwalkusz1@gmail.com>
2019-02-24 12:01:34 +0100

    Deployment

--- deployment/gcr/build-containers.sh
+++ deployment/gcr/build-containers.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -t gcr.io/metal-mariner-231915/justdoit-service -f service/Dockerfile service
+docker build -t gcr.io/metal-mariner-231915/justdoit-service:$TRAVIS_COMMIT -f service/Dockerfile service
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > gcloud-service-key.json
 gcloud auth activate-service-account --key-file gcloud-service-key.json
@@ -13,3 +13,4 @@ gcloud --quiet config set compute/zone europe-west2-a
 gcloud --quiet container clusters get-credentials todo-app-kubernetes
 
 gcloud docker -- push gcr.io/metal-mariner-231915/justdoit-service
+yes | gcloud beta container images add-tag gcr.io/metal-mariner-231915/justdoit-service:$TRAVIS_COMMIT gcr.io/metal-mariner-231915/justdoit-service:latest
